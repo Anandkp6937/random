a1259f75ff2a5a92f77cc5c367ee355bb2437e4d
anandkp6937 <anandkp6937@gmail.com>
2025-03-16 15:30:17 +0530

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const app=express();
 const dotenv=require('dotenv');
+const cors=require('cors')
+app.use(cors());
 dotenv.config();
 const PORT=8051 || process.env.PORT ;
 
